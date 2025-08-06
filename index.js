567b76529c5b54ee1b54c16f1d798348cd774457
Roniiwww <ronosmani29@gmail.com>
2025-08-06 03:14:10 +0200

    Change server port to 10000

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get("/", (req, res) => {
   res.send("Public homepage!");
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
